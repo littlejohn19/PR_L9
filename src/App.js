d19f8e5f1e80e3ed452cefcad887c801ed3413aa
Bartłomiej Mirek <bwmirek@gmail.com>
2021-05-07 09:58:44 +0200

    lab9

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Posts from "./components/posts";
 import NotFound from "./components/notFound";
 import Home from "./components/home";
 import NavBar from "./components/common/navbar";
+import LoginForm from "./components/loginForm";
 
 function App() {
   return (
@@ -11,13 +12,13 @@ function App() {
         <NavBar />
         <div className="container">
             <div className="content">
-                <Switch>
-                    <Route path="/posts"
-                    component={Posts}/>
-                    <Route path="/not-found" component={NotFound} />
-                    <Route path="/" exact component={Home} />
-                    <Redirect to="/not-found" />
-                </Switch>
+              <Switch>
+                <Route path="/login" component={LoginForm} />
+                <Route path="/posts" component={Posts}/>
+                <Route path="/not-found" component={NotFound} />
+                <Route path="/" exact component={Home} />
+                <Redirect to="/not-found" />
+              </Switch>
             </div>
         </div>
       </div>
